11559c6fd19dd24ff7f8e67aace52fa7cb06484b
alexjones <ajonzy@gmail.com>
2021-08-11 21:09:38 -0600

    Added User table

--- app.py
+++ app.py
@@ -5,13 +5,30 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config[ "SQLALCHEMY_DATABASE_URI"] = ""
+app.config[ "SQLALCHEMY_DATABASE_URI"] = "postgresql://utskvbidctbsrq:f590bf9df07389224eba2d12e89d7464e6c595eecc2482c4821132d39d3b40ec@ec2-34-204-128-77.compute-1.amazonaws.com:5432/dae2joimg0snjm"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
 
 CORS(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username")
+
+user_schema = UserSchema()
+multiple_user_schema = UserSchema(many=True)
 
 
 
