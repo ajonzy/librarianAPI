9ca98572f99050277bf73ee4093507b0c58a52c0
Alex Jones <ajonzy@gmail.com>
2021-08-12 20:27:09 -0600

    Updated user class to use token authentication

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
+import random
+import string
+
 app = Flask(__name__)
 app.config[ "SQLALCHEMY_DATABASE_URI"] = "postgresql://utskvbidctbsrq:f590bf9df07389224eba2d12e89d7464e6c595eecc2482c4821132d39d3b40ec@ec2-34-204-128-77.compute-1.amazonaws.com:5432/dae2joimg0snjm"
 
@@ -18,6 +21,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
+    token = db.Column(db.String, unique=True)
 
     def __init__(self, username, password):
         self.username = username
@@ -25,10 +29,18 @@ class User(db.Model):
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id", "username")
+        fields = ("id", "username", "token")
 
 user_schema = UserSchema()
 multiple_user_schema = UserSchema(many=True)
+
+
+def generate_token() {
+    token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+    while db.session.query(User).filter(User.token == token).first() != None:
+        token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+    return token
+}
 
 
 @app.route("/user/add", methods=["POST"])
@@ -41,8 +53,9 @@ def add_user():
     password = post_data.get("password")
 
     encrypted_password = bcrypt.generate_password_hash(password)
+    token = generate_token()
 
-    new_record = User(username, encrypted_password)
+    new_record = User(username, encrypted_password, token)
     db.session.add(new_record)
     db.session.commit()
 
@@ -53,14 +66,14 @@ def get_all_users():
     all_users = db.session.query(User).all()
     return jsonify(multiple_user_schema.dump(all_users))
 
-@app.route("/user/get/id/<id>", methods=["GET"])
-def get_user_by_id(id):
-    user = db.session.query(User).filter(User.id == id).first()
-    return jsonify(user_schema.dump(user))
+@app.route("/user/get/<token>", methods=["GET"])
+def get_user_by_id(token):
+    user = db.session.query(User).filter(User.token == token).first()
 
-@app.route("user/get/username/<username>", methods=["GET"])
-def get_user_by_username(username):
-    user = db.session.query(User).filter(User.username == username).first()
+    new_token = generate_token()
+    user.token = new_token
+    db.session.commit()
+
     return jsonify(user_schema.dump(user))
 
 
